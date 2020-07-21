3623724395fcf2639522985c3c08044dbb414432
Adrik1137 <63607833+Adrik1137@users.noreply.github.com>
2020-07-21 11:08:58 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-13---1",
+      "name": "13 - 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.13246301948317196,
+        "pitch": 0.11894691278419067,
+        "fov": 1.3145705460316273
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.46122613320331496,
+          "pitch": 0.05733852698947395,
+          "rotation": 5.497787143782138,
+          "target": "1-3---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-3---3",
+      "name": "3 - 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.3019004687372817,
+        "pitch": 0.14712599988735242,
+        "fov": 1.3145705460316273
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.341598921518667,
+          "pitch": -0.0005247018191649744,
+          "rotation": 0,
+          "target": "0-13---1"
+        },
+        {
+          "yaw": 1.846277729643317,
+          "pitch": -0.0985795260696598,
+          "rotation": 0,
+          "target": "2-11---1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-11---1",
+      "name": "11 - 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.06239932972127882,
+        "pitch": -0.16864856187428146,
+        "fov": 1.3145705460316273
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
